ccd2ffb9716b64badec54b9a167c902706575700
vdv7 <vdv7@users.noreply.github.com>
2023-04-03 09:34:40 -0400

    app.start app.event

--- screenbuilder.js
+++ screenbuilder.js
@@ -6,7 +6,7 @@
 //      Add Component should only be visible when clicked object is a bin
 
 
-logToConsole();
+// logToConsole();
 
 var curDate=(new Date()),curDateTime=Math.floor(curDate.getTime()/1000)-curDate.getTimezoneOffset()*60;
 
@@ -24,80 +24,80 @@ var COMPONENTS = new Map([
 
 var lastValidUINLcode=`{"v":[\n  \n]}\n`;
 
+// on start (i.e., once handshake event is received)
+app.start(event=>{
+    app.display({
+        cap:'UINL screen builder',
+        require:{c:['grid','opt']},
+        style:'style.css',
+        value:[
+            {id:"Preview"},
+            {id:"UINL",v:[
+                {id:'UINLcode',cap:'',v:lastValidUINLcode,in:1,on:{v:[]}},
+                {id:'Clean up',class:'btn'},
+                {id:'Save',class:'btn'},
+            ]},
+            {id:"Add Component",v:[...COMPONENTS.keys()].map(x=>({id:x,c:'btn'}))},
+        ]
+    });
+});
 
-function userEvent(event){
-
-    if(event.t==0){  // on handshake
-        app.display({
-            cap:'UINL screen builder',
-            require:{c:['grid','opt']},
-            style:'style.css',
-            value:[
-                {id:"Preview"},
-                {id:"UINL",v:[
-                    {id:'UINLcode',cap:'',v:lastValidUINLcode,in:1,on:{v:[]}},
-                    {id:'Clean up',class:'btn'},
-                    {id:'Save',class:'btn'},
-                ]},
-                {id:"Add Component",v:[...COMPONENTS.keys()].map(x=>({id:x,c:'btn'}))},
-            ]
-        });
-
-    }else{ // on any user event other than handshake
-        var itemId=event.u,
-            itemValue=event.v,
-            displayUpdates=[{U:'Warning',v:null}];
-        
-        if(itemId=='UINLcode'){ // if user is editing UINL directly
-            try{
-                let preview=JSON.parse(itemValue);
-                if(preview.constructor===Object){
-                    itemValue=JSON.stringify(preview,null,2);
-                    if(itemValue!==lastValidUINLcode){
-                        lastValidUINLcode=itemValue;
-                        preview.id='Preview';
-                        preview.i=0;
-                        displayUpdates.push({U:'Preview',v:null},{add:[preview]});
-                    }
-                }else{
-                    displayUpdates.push({U:'UINL',add:[{id:'Warning',v:'APP→UI message must be a JSON object.'}]});
-                }
-            }catch(e){
-                if(e.message.includes('JSON')){
-                    displayUpdates.push({U:'UINL',add:[{id:'Warning',v:'Invalid JSON string.'}]});
-                }else{
-                    throw(e);
-                }
+// when UINLcode item is edited
+app.event({u:'UINLcode'}, event=>{
+    var displayUpdates=[{U:'Warning',v:null}];
+    try{
+        let uinlCode = event.v;
+        let preview=JSON.parse(uinlCode);
+        if(preview.constructor===Object){
+            uinlCode=JSON.stringify(preview,null,2);
+            if(uinlCode!==lastValidUINLcode){
+                lastValidUINLcode=uinlCode;
+                preview.id='Preview';
+                preview.i=0;
+                displayUpdates.push({U:'Preview',v:null},{add:[preview]});
             }
-
-        }else if(itemId==='Clean up'){ // if user clicks Clean up
-            if(lastValidUINLcode){
-                displayUpdates.push({U:'UINLcode',v:lastValidUINLcode});
-            }
-
-        }else if(itemId==='Save'){ // if user clicks Save
-            if(lastValidUINLcode){
-                displayUpdates.push({save:['screenBuilder.uinl',lastValidUINLcode]});
-            }
-
-        }else if(COMPONENTS.has(itemId)){ // check if one of the COMPONENTS buttons was clicked
-            let preview=JSON.parse(lastValidUINLcode);
-            if(!preview.v || preview.v.constructor!==Array)preview.v=[];
-            preview.v.push(COMPONENTS.get(itemId));
-            lastValidUINLcode=JSON.stringify(preview,null,2);
-            displayUpdates.push({U:'UINLcode',v:lastValidUINLcode});
-            preview.id='Preview';
-            preview.i=0;
-            // uinl.df={on:{pc:[]}};
-            displayUpdates.push({U:'Preview',v:null},{add:[preview]});
-
-        // }else if(event.pc){ // check for point-click event
-        //     console.log(event);
-            
-
-        // }else{ // if one of the preview components is toggled/edited
-            
+        }else{
+            displayUpdates.push({U:'UINL',add:[{id:'Warning',v:'APP→UI message must be a JSON object.'}]});
         }
-        app.display({Q:displayUpdates});
+    }catch(e){
+        if(e.message.includes('JSON')){
+            displayUpdates.push({U:'UINL',add:[{id:'Warning',v:'Invalid JSON string.'}]});
+        }else{
+            throw(e);
+        }
     }
-}
+    app.display({Q:displayUpdates});
+});
+
+// when Clean up button is clicked
+app.event({u:'Clean up'}, event=>{
+    app.display({Q:[
+        {U:'Warning',v:null},
+        {U:'UINLcode',v:lastValidUINLcode}
+    ]});
+});
+
+// when Save button is clicked
+app.event({u:'Save'},event=>{
+    app.display({Q:[
+        {U:'Warning',v:null},
+        {save:['screenBuilder.uinl',lastValidUINLcode]}
+    ]});
+});
+
+// any other event (e.g., when one of the COMPONENTS buttons is clicked)
+app.event({}, event=>{
+    var itemId=event.u;
+    var displayUpdates=[{U:'Warning',v:null}];
+    if(COMPONENTS.has(itemId)){ // check if one of the COMPONENTS buttons was clicked
+        let preview=JSON.parse(lastValidUINLcode);
+        if(!preview.v || preview.v.constructor!==Array)preview.v=[];
+        preview.v.push(COMPONENTS.get(itemId));
+        lastValidUINLcode=JSON.stringify(preview,null,2);
+        displayUpdates.push({U:'UINLcode',v:lastValidUINLcode});
+        preview.id='Preview';
+        preview.i=0;
+        displayUpdates.push({U:'Preview',v:null},{add:[preview]});        
+    }
+    app.display({Q:displayUpdates});
+});
